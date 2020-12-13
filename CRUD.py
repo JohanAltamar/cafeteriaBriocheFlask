92044edd64cf457e2681633ef10dcbb49d62134d
carcasvar <carcasvar@gmail.com>
2020-12-13 18:45:03 -0500

    Crear y editar productos

--- CRUD.py
+++ CRUD.py
@@ -2,7 +2,7 @@ import db
 def register(usuario,clave,email):
     conexion = db.get_db()
     cur=conexion.cursor()
-    cur.executescript("INSERT INTO users (username, email, password) VALUES ('%s','%s','%s')" % (usuario, email, clave))
+    cur.executescript("INSERT INTO users (username, email, password, enabled) VALUES ('%s','%s','%s','True')" % (usuario, email, clave))
     conexion.commit()
     db.close_db()
 
@@ -29,5 +29,39 @@ def actualizar_usuario(id,usuario,clave,email,enabled):
         cur.executescript("UPDATE users SET username='%s', email='%s', enabled='%s' WHERE id='%s'" % (usuario, email, enabled, id))
     else:
         cur.executescript("UPDATE users SET username='%s', email='%s', password='%s', enabled='%s' WHERE id='%s'" % (usuario, email, clave, enabled, id))
+    conexion.commit()
+    db.close_db()
+
+def create_product(product_name,product_price,filename):
+    conexion = db.get_db()
+    cur=conexion.cursor()
+    cur.executescript("INSERT INTO products (product_name, product_price, product_filename,enabled) VALUES ('%s','%s','%s','True')" % (product_name, product_price, filename))
+    conexion.commit()
+    db.close_db()
+
+def leer_productos():
+    conexion = db.get_db()
+    cur=conexion.cursor()
+    cur.execute("SELECT * FROM products")
+    rv = cur.fetchall()
+    db.close_db()
+    return rv
+
+def buscar_un_producto(product_name):
+    conexion = db.get_db()
+    cur=conexion.cursor()
+    cur.execute("SELECT * FROM products WHERE product_name = '" + product_name + "'")
+    rv = cur.fetchone()
+    db.close_db()
+    print(rv)
+    return rv
+
+def actualizar_producto(id,product_name,product_price,product_filename,enabled):
+    conexion = db.get_db()
+    cur=conexion.cursor()
+    if product_filename == "":
+        cur.executescript("UPDATE products SET product_name='%s', product_price='%s', enabled='%s' WHERE id='%s'" % (product_name, product_price, enabled, id))
+    else:
+        cur.executescript("UPDATE products SET product_name='%s', product_price='%s', product_filename='%s', enabled='%s' WHERE id='%s'" % (product_name, product_price, product_filename,enabled, id))
     conexion.commit()
     db.close_db()
--- bin/start.sh
+++ bin/start.sh
@@ -3,4 +3,7 @@
 export FLASK_APP=app.py
 export FLASK_ENV=development
 
+# PARA POWERSHELL EN WINDOWS:
+$env:FLASK_APP = "app.py"
+$env:FLASK_ENV = "development"
 flask run --port 5000
